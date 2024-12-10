20cd76fd2a863797a1d4d86916518c6809fc043b
Nolan Boxill <nolan.donb@gmail.com>
2024-12-09 23:45:09 -0700

    cleanup

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Import router here
-
-// Create the Vue app and use the router
-createApp(App)
-  .use(router)  // Use the router in the app
-  .mount('#app');  // Mount the app to the DOM
